d3c78f77adebd97d4447c2190335ece80c489a1b
Chris Gregg <cgregg@cs.tufts.edu>
2016-06-22 17:38:24 -0400

    added new activity

---
+++ app/src/main/java/com/cocoadrillosoftware/rosterquiz/LoadStudentsFromWebsite.java
@@ -0,0 +1,13 @@
+package com.cocoadrillosoftware.rosterquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoadStudentsFromWebsite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_students_from_website);
+    }
+}
